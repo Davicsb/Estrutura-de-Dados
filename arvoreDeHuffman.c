72ebf2dc8ba2a514bbbf67d165f633b28d940404
Tenorio <jvtv@ic.ufal.br>
2024-10-12 23:48:45 -0300

    Update arvoreDeHuffman.c
    up & date

--- arvoreDeHuffman.c
+++ arvoreDeHuffman.c
@@ -5,10 +5,11 @@
 
 #define LER_BINARIO "rb"
 #define ESCREVER_BINARIO "wb"
-#define ITEM_PARA_O_NO_PAI NULL
-//nas especificações o professor diz q o nó que é pai tem que ser salvo como * no arquivo, vou perguntar pra ele se dentro do programa eu posso usar NULL
+#define TAM 256
 
 typedef struct node no;
+typedef long long int LLi;
+typedef unsigned char unC;
 
 struct node {
     void* item;
@@ -24,119 +25,59 @@ int taVazio(no *head){
     return (head == NULL);
 }
 
+//retorna 1 se os dois itens (unsigned char) forem iguais
 int unsignedCharEquals(void *item1, void *item2){
     return (*(unsigned char*) item1 == *(unsigned char*) item2);
 }
 
-//swapL alterado pra dar swap nos filhos também.
-void swapL(no *a, no *b){
-    void *lixo = a->item;
-    int lixo2 = a->freq;
-    no *filhos[2] = {a->left, a->right};
+no* add(no *head, no *new){
 
-    a->item = b->item;
-    a->freq = b->freq;
-    a->left = b->left;
-    a->right = b->right;
+    if(taVazio(head)){
+        head = new;
 
-    b->item = lixo;
-    b->freq = lixo2;
-    b->left = filhos[0];
-    b->right = filhos[1];
-}
+    } else if(new->freq <= head->freq){
+        new->next = head;
+        head = new;
 
-no* add(no *head, void *item){
-
-    if(!taVazio(head)){
+    } else {
         no *lixo = head;
-
-        while(lixo){
-            if(unsignedCharEquals(lixo->item, item)){
-                lixo->freq += 1;
-                
-                return head;
-            }
-
+        while(lixo->next && lixo->next->freq < new->freq){
             lixo = lixo->next;
         }
+        new->next = lixo->next;
+        lixo->next = new;
+
     }
 
-    no *novoNo = (no*) malloc(sizeof(no));
-    //checagem de erro
-    if(novoNo == NULL){
-        printf("Falha ao alocar memória na função add\n");
-        exit(-1);
-    }
-    novoNo->item = item;
-    novoNo->freq = 1;
-    novoNo->left = NULL;
-    novoNo->right = NULL;
-    novoNo->next = head;
-
-    return novoNo;
+    return head;
 
 }
 
-void bubbleSort(no *head){
-    no *aux = head;
-    while(aux){
-
-        no *atual = head;
-
-        while(atual->next != NULL){
+void inserir(no **head, LLi freq[TAM]){
+    for(int i = 0; i < TAM; i++){
+        if(freq[i] > 0){
+            no* newNo = (no*) malloc(sizeof(no));
             
-            if(atual->freq > atual->next->freq){
-                swapL(atual, atual->next);
-            }
+            newNo->item = malloc(sizeof(unC));
+            *(unC*)newNo->item = (unC)i;
+            newNo->freq = freq[i];
+            newNo->next = NULL;
+            newNo->left = NULL;
+            newNo->right = NULL;
 
-            atual = atual->next;
+            *head = add(*head, newNo);
         }
-
-        aux = aux->next;
     }
 }
 
-//arvore
-//entrada: cabeça de uma lista encadeada
-//saída: a raíz de uma arvore de huffman
-//função: 
-//1: checa se a lista existe, e se há mais de um elemento nela (se falhar ela retorna a cabeça), se houver mais de um elemento ela já ordena a lista também.
-//2: se houver mais de um elemento, ela vai criar um novo nó que tera os dois primeiros nós (que são os que tem a menor frequencia) como filhos (juntando suas frequencias) e os subistituira na lista.
-//3: como esse novo nó subistituiu a cabeça, a função ocorrerá recursivamente até só ter um nó (que será a raíz da arvore)
-no* arvore(no* head){
-    if(head){
-
-        if(head->next != NULL){
-            bubbleSort(head);
-
-            no *novoNo = (no*) malloc(sizeof(no));
-            //checagem de erro
-            if(novoNo == NULL){
-                printf("Falha ao alocar memória na função arvore\n");
-                exit(-1);
-            }
-
-            novoNo->item = ITEM_PARA_O_NO_PAI;
-            novoNo->freq = head->freq + head->next->freq;
-            novoNo->next = head->next->next;
-
-            novoNo->left = head;
-            novoNo->right = head->next;
-
-            head = novoNo;
-
-            return arvore(head);
-        
-        } else {
-            return head;
-        }
-
+void printar(no *head){
+    no *lixo = head;
+    while(lixo){
+        printf("O byte é 0x%02x (%c) com %d ocorrências\n", *(unC*)lixo->item, *(unC*)lixo->item, lixo->freq);
+        lixo = lixo->next;
     }
-
 }
-//printPreOrder
-//entrada: raíz de uma arvore de huffman
-//função: imprime em pre ordem a arvore, se o nó não for uma FOLHA (nó que não tem filho) o char será impresso, caso contrário * será impresso.
+
 void printPreOrder(no *head){
     if(head != NULL){
 
@@ -152,42 +93,81 @@ void printPreOrder(no *head){
     }
 }
 
+no* arvore(no* head){
+    if(head){
+
+        if(head->next != NULL){
+
+            no *novoNo = (no*) malloc(sizeof(no));
+            //checagem de erro
+            if(novoNo == NULL){
+                printf("Falha ao alocar memória na função arvore\n");
+                exit(-1);
+            }
+
+            novoNo->item = NULL;
+            novoNo->freq = head->freq + head->next->freq;
+            novoNo->next = NULL;
+
+            novoNo->left = head;
+            novoNo->right = head->next;
+
+            head = head->next->next;
+
+            head = add(head, novoNo);
+            return arvore(head);
+        
+        } else {
+            return head;
+        }
+
+    }
+}
+
+no* desenfilar(no *head){
+    no *lixo = NULL;
+    if(head){
+        lixo = head;
+        head = lixo->next;
+        lixo->next = NULL;
+    }
+
+    return lixo;
+}
+
 int main(){
     printf("Qual o nome do arquivo? (inclua sua extensão)\n");
 
-    char nomeArquivo[FILENAME_MAX];
+    char nomeArquivo[1024];
     scanf(" %[^\n]", nomeArquivo);
 
     FILE* arquivo = fopen(nomeArquivo, LER_BINARIO);
-    //checagem de erro
     if (arquivo == NULL) {
         printf("Falha ao abrir o arquivo.\n");
         exit(-1);
     }
 
     fseek(arquivo, 0, SEEK_END);
-    long long tamanhoArquivo = ftell(arquivo);
-    //checagem de erro
-    if(tamanhoArquivo == 0 || tamanhoArquivo == -1){
-        printf("Falha ao pegar o tamanho do arquivo ou arquivo vazio.\n");
-        exit(-1);
-    }
+    LLi tamanhoArquivo = ftell(arquivo);
     fseek(arquivo, 0, SEEK_SET);
 
     unsigned char* dados = (unsigned char*) malloc(tamanhoArquivo);
-    //checagem de erro
     if(dados == NULL){
         printf("Falha ao alocar memória.");
         exit(-1);
     }
 
     fread(dados, 1, tamanhoArquivo, arquivo);
+    LLi freq[TAM] = {0};
 
     no *listaFreq = criarLista();
-    for (unsigned long i = 0; i < tamanhoArquivo; i++) {
-        listaFreq =  add(listaFreq, &dados[i]);
+    for (LLi i = 0; i < tamanhoArquivo; i++) {
+        freq[dados[i]] += 1;
     }
 
+    inserir(&listaFreq, freq);
+
+    printar(listaFreq);
     listaFreq = arvore(listaFreq);
     printPreOrder(listaFreq);
     printf("\n");

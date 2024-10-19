9bce331a74177e9c9586aef13755c9ad4254fb27
Davicsb <144847037+Davicsb@users.noreply.github.com>
2024-10-19 17:13:17 -0300

    Add files via upload

---
+++ Huffman/Code/descompactacao.h
@@ -0,0 +1,22 @@
+#ifndef DESCOMPACTACAO_H
+#define DESCOMPACTACAO_H
+
+#include "compactacao.h"
+
+typedef short int sInt;
+
+void *criarPonteiro(unC byte);
+
+no *criarNo(void *byte);
+
+int readTrash(FILE *arquivo);
+
+LLi readSizeTree(FILE *arquivo);
+
+no* readTree(FILE *arquivo, LLi *treeSize);
+
+void writeFile(FILE *arquivoIn, FILE *arquivoOut, short int trash, LLi sizeFile, no *tree);
+
+int processoParaDescompactar(char *nomeDoArquivo);
+
+#endif

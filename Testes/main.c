bb0110c0e0a35f5db579255a896de3a2cf0fc530
Davicsb <144847037+Davicsb@users.noreply.github.com>
2024-10-21 23:02:25 -0300

    Add files via upload

---
+++ Testes/main.c
@@ -0,0 +1,61 @@
+#include "Code/compactacao.h"
+#include "Code/descompactacao.h"
+
+int main() {
+    limpartela();
+    
+    while (1)
+    {
+        setlocale(LC_ALL, "pt_BR.UTF-8");
+        introducao();
+
+        int opcao;
+        scanf("%d", &opcao);
+        limpartela();
+
+        if(opcao == 1)
+        {
+        
+        char *nomeDoArquivo = pegarNomeDoArquivo();
+
+        limpartela();
+
+            if (processoParaCompactar(nomeDoArquivo) == 1)
+            {
+                printf("> Arquivo compactado com sucesso! <\n");
+            }
+            else
+            {
+                printf("> Falha ao abrir \"%s\". Tente novamente! <\n", nomeDoArquivo);
+            }
+        
+        }
+
+        else if(opcao == 2)
+        {
+
+        char *nomeDoArquivo = pegarNomeDoArquivo();
+
+        limpartela();
+
+            if (processoParaDescompactar(nomeDoArquivo) == 1)
+            {
+                
+                printf("> Arquivo descompactado com sucesso! <\n");
+            }
+            else
+            {
+                printf("> Falha ao abrir \"%s\". Tente novamente! <\n", nomeDoArquivo);
+            }
+
+        }
+
+        else 
+        {
+            limpartela();
+            break;
+        }
+    }
+
+    return 0;
+}
